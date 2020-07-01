73437b198202b70e5a43757068f4e8c9fde0f867
rolando <rolando@rolando-Inspiron-5584>
2020-07-01 14:54:18 -0500

    Se crea el Objeto BaseDAO

---
+++ softcoatl/BaseDAO.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * BaseDAO
+ * FactTequitl®
+ * © 2020, Softcoatl
+ * http://www.softcoatl.mx
+ * @author Rolando Esquivel Villafaña, Softcoatl
+ * @version 1.0
+ * @since jul 2020
+ */
+
+namespace com\softcoatl\utils;
+
+use com\softcoatl\utils\IConnection;
+
+class BaseDAO {
+
+    /* @var $conn \mysqli */
+    protected $conn;
+
+    public function __construct() {
+        $this->conn = IConnection::getConnection();
+    }
+
+    public function __destruct() {
+        $this->conn->close();
+    }
+}
