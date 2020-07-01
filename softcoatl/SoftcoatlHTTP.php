82c5aff39d0fd15152e9bac0fbb5a961d31e1b33
rolando <rolando@rolando-Inspiron-5584>
2020-07-01 14:56:22 -0500

    Se crea el Objeto BaseDAO

--- softcoatl/SoftcoatlHTTP.php
+++ softcoatl/SoftcoatlHTTP.php
@@ -16,4 +16,5 @@ require_once(dirname(__FILE__) . "/PDOConnection.php");
 require_once(dirname(__FILE__) . "/QueryParameters.php");
 require_once(dirname(__FILE__) . "/Request.php");
 require_once(dirname(__FILE__) . "/Utils.php");
+require_once(dirname(__FILE__) . "/BaseDAO.php");
 require_once(dirname(__FILE__) . "/BaseVO.php");
